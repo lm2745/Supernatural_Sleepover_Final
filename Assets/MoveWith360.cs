00367241f6b5a80035510cdb100e064b51eeedc2
Samir Mohamed <samirmohamedcs@gmail.com>
2015-11-17 13:45:37 -0500

    Sample character follows analog stick input with rotation and translation

---
+++ Assets/MoveWith360.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWith360 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	float range = 10;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float h = Input.GetAxis("360LeftX");
+		float xpos = h * range;
+
+		float y = Input.GetAxis ("360LeftY");
+		float ypos = y * range;
+
+		transform.position = new Vector3(xpos, -ypos, 0);
+		Debug.Log ("XAxis: " + h + "  XPos: " + xpos + "   YAxis: " + y + "  YPos " + ypos);
+
+	}
+}
