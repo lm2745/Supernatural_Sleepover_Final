1c5ed73e67763afb9a2342363d4fa237345e81c2
SamirMohamedCS <SamirMohamedCS@gmail.com>
2015-12-08 01:43:39 -0500

    Actually synced with master now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,9 @@ public class Player : MonoBehaviour {
     //             getAction1(), getAction1Down(), getAction1Up()
     //             getAction2(), getAction2Down(), getAction2Up()
     public float moveSpeed;
-<<<<<<< HEAD
-    public float maxSpeed = 5f;
-=======
 	/* samir physics movement commit */
 	public float maxSpeed = 8f;
 	// ******
->>>>>>> d0ad9eb6901e373bed40056f9d930afdf4ca17ff
     //public float health;
 	public Health health;
 
@@ -29,30 +25,6 @@ public class Player : MonoBehaviour {
     void Start ()
     {
 		//createManager(1, OS.WINDOWS);
-<<<<<<< HEAD
-        moveSpeed = 0.2f;
-        if (classID == 1)
-        {
-            GetComponent<Rigidbody>().mass = .4f;
-        }
-        else if (classID == 2)
-        {
-            GetComponent<Rigidbody>().mass = .3f;
-        }
-        else if (classID == 2)
-        {
-            GetComponent<Rigidbody>().mass = .32f;
-        }
-        else if (classID == 2)
-        {
-            GetComponent<Rigidbody>().mass = .3f;
-        }
-    }
-	
-	void FixedUpdate() {
-		//if (inputManager != null) {
-			moveWithForce();
-=======
         //moveSpeed = 0.1f;
 	}
 	
@@ -61,63 +33,11 @@ public class Player : MonoBehaviour {
 		//move();
 		moveWithForce();
 		timeSinceLastAttack += Time.deltaTime;
->>>>>>> d0ad9eb6901e373bed40056f9d930afdf4ca17ff
 		//}
 	}
 
 	void Update () {
 		if (inputManager.getAction1Down() ) {
-<<<<<<< HEAD
-			//if (tag == "Santa") {
-			if (classID == 1) {
-				Projectile temp = (Projectile) Instantiate(projectilePrefab, transform.position, transform.rotation);
-				temp.transform.position += temp.transform.forward;
-				temp.damage = 50f;
-				temp.projectileSpeed = 0.3f;
-				temp.maxProjectileLife = 5f;
-				temp.knockback = 500f;
-				temp.team = team;
-                //health.decreaseHealth(50f);
-            }
-			//else if (tag == "Cupid") {
-			else if (classID == 2) {
-				Projectile temp = (Projectile) Instantiate(projectilePrefab, transform.position, transform.rotation);
-				temp.transform.position += temp.transform.forward;
-				temp.damage = 10f;
-				temp.projectileSpeed = 0.15f;
-				temp.maxProjectileLife = 50f;
-				temp.knockback = 50f;
-				temp.team = team;
-
-                //health.decreaseHealth(10f);
-            }
-            //else if (tag == "Easter Bunny") {
-            else if (classID == 3) {
-				Projectile temp = (Projectile) Instantiate(projectilePrefab, transform.position, transform.rotation);
-				temp.transform.position += temp.transform.forward;
-				temp.damage = 30f;
-				temp.projectileSpeed = 0.3f;
-				temp.maxProjectileLife = 5f;
-				temp.knockback = 200f;
-				temp.team = team;
-
-                //health.decreaseHealth(30f);
-            }
-            //else if (tag == "Tooth Fairy") {
-            else if (classID == 4) {
-				Projectile temp = (Projectile) Instantiate(projectilePrefab, transform.position, transform.rotation);
-				temp.transform.position += temp.transform.forward;
-				temp.damage = 5f;
-				temp.projectileSpeed = 0.25f;
-				temp.maxProjectileLife = 100f;
-				temp.knockback = 10f;
-				temp.team = team;
-
-                //health.decreaseHealth(5f);
-            }
-            Debug.Log(GetComponent<Rigidbody>().mass);
-        }
-=======
 			//attack cooldown (temp code assuming cooldown same for all classes)
 			if (timeSinceLastAttack >= attackCooldown) {
 				//if (tag == "Santa") {
@@ -174,7 +94,6 @@ public class Player : MonoBehaviour {
 				timeSinceLastAttack = 0f;
 			}
 		}
->>>>>>> d0ad9eb6901e373bed40056f9d930afdf4ca17ff
 
 		//test setting player class
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
@@ -285,25 +204,6 @@ public class Player : MonoBehaviour {
         }
     }
 
-<<<<<<< HEAD
-    public void moveWithForce()
-    {
-        float h = inputManager.getAxisX();
-
-        float y = inputManager.getAxisY();
-
-
-        Vector3 direction = new Vector3(h, 0, -y);
-
-        if (direction != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-            GetComponent<Rigidbody>().AddForce(transform.forward * 8) ;
-            GetComponent<Rigidbody>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody>().velocity, maxSpeed);
-            //transform.position += transform.forward * Vector3.Distance(Vector3.zero, direction) * moveSpeed;
-        }
-    }
-=======
 	public void moveWithForce()
 	{
 		float h = inputManager.getAxisX();
@@ -340,5 +240,4 @@ public class Player : MonoBehaviour {
 			//transform.position += transform.forward * Vector3.Distance(Vector3.zero, direction) * moveSpeed;
 		}
 	}
->>>>>>> d0ad9eb6901e373bed40056f9d930afdf4ca17ff
 }
