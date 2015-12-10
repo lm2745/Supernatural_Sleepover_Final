e1c853b7c6f609e08f473e004ddcb4be2bef67ad
Fay Koh <fay.koh@nyu.edu>
2015-12-10 15:32:13 -0500

    more advanced restart code that keeps track of how many rounds each team has won

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -4,32 +4,47 @@ using UnityEngine.UI;
 
 public class Restart : MonoBehaviour {
 
+	static int teamOneCount = 0;
+	static int teamTwoCount = 0;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+
+		string textBuffer = "";
+		
+		if (GameManager.winningTeam == true)
+		{
+			teamOneCount += 1;
+			textBuffer += "\nTeam One (Santa and Easter Bunny) Wins!";
+		}
+		else
+		{
+			teamTwoCount += 1;
+			textBuffer += "\nTeam Two (Cupid and Tooth Fairy) Wins!";
+		}
+		
+		textBuffer += "\nTeam One Rounds Won: " + teamOneCount;
+		textBuffer += "\nTeam Two Rounds Won: " + teamTwoCount;
+
+		GetComponent<Text>().text = textBuffer;
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
-		string textBuffer = "";
 		
-		if (GameManager.winningTeam == false)
-		{
-			textBuffer += "\nTeam Two (Cupid and Tooth Fairy) Wins!";
-		}
-		else
-		{
-			textBuffer += "\nTeam One (Santa and Easter Bunny) Wins!";
-		}
-
 		if (Input.GetKeyDown (KeyCode.R))
 		{
 			//Application.LoadLevel("gameUI"); // Application.LoadLevel("myGameScene");
 			Application.LoadLevel ("differentArenaGameScene");
 		}
-
-		GetComponent<Text>().text = textBuffer;
+		else if (Input.GetKeyDown (KeyCode.O))
+		{
+			teamOneCount = 0;
+			teamTwoCount = 0;
+			Application.LoadLevel ("differentArenaGameScene");
+		}
 	}
 }
