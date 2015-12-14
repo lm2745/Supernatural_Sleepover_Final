23b35099312071686c1e47e3482bef01ac7597d6
Fay Koh <fay.koh@nyu.edu>
2015-12-14 11:20:44 -0500

    started on character select screen

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NextLevelButton(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+
+	public void OnGUI()
+	{
+		if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+			Application.LoadLevel("level2");
+	}
+}
---
+++ Assets/Scripts/NextLevelButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelButton : MonoBehaviour
+{
+
+	public void levelnameNum(int index)
+	{
+		Application.LoadLevel(index);
+	}
+		
+	public void levelnameString(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+}
