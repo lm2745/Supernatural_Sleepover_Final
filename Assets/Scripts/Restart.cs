661e65651f009cea726c80acfb6468e693787d41
Mila Mikhitarova <lm2745@nyu.edu>
2015-12-14 19:55:00 -0500

    Got Sounds

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -4,32 +4,47 @@ using UnityEngine.UI;
 
 public class Restart : MonoBehaviour {
 
+	static int teamOneCount = 0;
+	static int teamTwoCount = 0;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+
+		string textBuffer = "";
+		
+		if (GameManager.winningTeam == true)
+		{
+			textBuffer += "\nTeam Two (Easter Bunny and Tooth Fairy) Wins!";
+		}
+		else
+		{
+			textBuffer += "\nTeam One (Santa and Cupid) Wins!";
+
+		}
+
+		textBuffer += "\n";
+		textBuffer += "\nRed Team Rounds Won: " + teamOneCount;
+		textBuffer += "\nBlue Team Rounds Won: " + teamTwoCount;
+
+		GetComponent<Text>().text = textBuffer;
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
-		string textBuffer = "";
 		
-		if (GameManager.winningTeam == false)
-		{
-			textBuffer += "\nTeam Two (Cupid and Tooth Fairy) Wins!";
-		}
-		else
-		{
-			textBuffer += "\nTeam One (Santa and Easter Bunny) Wins!";
-		}
-
 		if (Input.GetKeyDown (KeyCode.R))
 		{
 			//Application.LoadLevel("gameUI"); // Application.LoadLevel("myGameScene");
 			Application.LoadLevel ("differentArenaGameScene");
 		}
-
-		GetComponent<Text>().text = textBuffer;
+		else if (Input.GetKeyDown (KeyCode.O))
+		{
+			teamOneCount = 0;
+			teamTwoCount = 0;
+			Application.LoadLevel ("differentArenaGameScene");
+		}
 	}
 }
